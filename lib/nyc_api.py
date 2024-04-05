21f94d4c219b36b18079fd3c03b65b5646509b3e
Dennis <ngetichdennis90@gmail.com>
2024-04-05 22:41:30 +0300

    completed the tsak

--- lib/nyc_api.py
+++ lib/nyc_api.py
@@ -3,6 +3,14 @@ import requests
 import json
 
 class GetPrograms:
+  def program_agencies(self):
+    # we use the JSON library to parse the API response into nicely formatted JSON
+        programs_list = []
+        programs = json.loads(self.get_programs())
+        for program in programs:
+            programs_list.append(program["agency"])
+
+        return programs_list
 
   def get_programs(self):
     URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
@@ -10,5 +18,10 @@ class GetPrograms:
     response = requests.get(URL)
     return response.content
 
-programs = GetPrograms().get_programs()
-print(programs)
+# programs = GetPrograms().get_programs()
+# print(programs)
+programs = GetPrograms()
+agencies = programs.program_agencies()
+
+for agency in set(agencies):
+    print(agency)
